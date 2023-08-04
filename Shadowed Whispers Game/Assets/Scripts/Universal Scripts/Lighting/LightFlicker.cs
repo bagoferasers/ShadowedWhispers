81561cd26051f9911745fa01aa5651f1538b196a
Colby Bailey <83721925+bagoferasers@users.noreply.github.com>
2023-08-04 11:40:42 -0600

    Menu Scene
    Updated look and light flicker script

---
+++ Shadowed Whispers Game/Assets/Scripts/Universal Scripts/Lighting/LightFlicker.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    private UnityEngine.Rendering.Universal.Light2D light2D;
+    private float flickerSpeed = 5.0f;
+    private float flickerRange = 0.5f;
+    private float intensity = 0.5f;
+    private WaitForSeconds waitTime;
+
+    void Start( )
+    {
+        light2D = this.GetComponent< UnityEngine.Rendering.Universal.Light2D >( );
+        waitTime = new WaitForSeconds( Random.Range( 0.1f, 10.0f ) );
+        StartCoroutine( FlickerCoroutine( ) );
+    }
+
+    private IEnumerator FlickerCoroutine( )
+    {
+        while( true ) 
+        {
+            yield return waitTime;
+            flickerSpeed = 0.1f;
+            intensity = 0.5f + Random.Range( 0.0f, 0.4f );
+            float elapsedTime = 0.0f;
+            while( elapsedTime < flickerSpeed )
+            {
+                elapsedTime += Time.deltaTime;
+                light2D.intensity = Mathf.PingPong( 1.0f, intensity );
+                yield return null;
+            }
+            light2D.intensity = 1.0f;
+            waitTime = new WaitForSeconds( Random.Range( 0.1f, 10.0f ) );
+        }
+    }
+}
