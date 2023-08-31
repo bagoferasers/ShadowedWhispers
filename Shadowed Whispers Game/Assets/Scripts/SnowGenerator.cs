2eb3af3adc9149c5da84de0463ea7c662b189f72
Colby Bailey <83721925+bagoferasers@users.noreply.github.com>
2023-08-30 20:59:56 -0600

    Added poles
    Love light poles

--- Shadowed Whispers Game/Assets/Scripts/SnowGenerator.cs
+++ Shadowed Whispers Game/Assets/Scripts/SnowGenerator.cs
@@ -8,6 +8,8 @@ public class SnowGenerator : MonoBehaviour
     private GameObject snowflakePrefab2;
     private GameObject snowflakePrefab3;
     private GameObject[] snowflakes;
+    private Transform snowflakeParent;
+    public float maxRotationSpeed = 30.0f;
     public float spawnInterval = 1.0f;
 
     // Start is called before the first frame update
@@ -16,6 +18,7 @@ public class SnowGenerator : MonoBehaviour
         snowflakePrefab1 = GameObject.Find( "Snowflake_1" );
         snowflakePrefab2 = GameObject.Find( "Snowflake_2" );
         snowflakePrefab3 = GameObject.Find( "Snowflake_3" );
+        snowflakeParent = this.transform;
         snowflakes = new GameObject[] { snowflakePrefab1, snowflakePrefab2, snowflakePrefab3 };
         StartCoroutine( GenerateSnow( ) );
     }
@@ -28,19 +31,21 @@ public class SnowGenerator : MonoBehaviour
             GameObject snowflakePrefab = snowflakes[ i ];
 
             // position in sky
-            Vector3 spawnPosition = new Vector3( Random.Range( -1f, 1f ), 0.3f, 0 );
+            Vector3 spawnPosition = new Vector3( Random.Range( -2f, 10f ), 0.5f, 0f );
             
             // create snowflake and add physics
             GameObject snowflake = Instantiate( snowflakePrefab, spawnPosition, Quaternion.identity );
+            snowflake.transform.parent = snowflakeParent;
             Rigidbody2D rb = snowflake.AddComponent< Rigidbody2D >( );
             if( rb != null )
             {
                 rb.velocity = Vector3.down * Random.Range( 0.01f, 0.1f );
                 rb.mass = 0.00001f;
-                rb.gravityScale = 0.00001f;                
+                rb.gravityScale = 0.00001f;              
+                rb.angularVelocity = Random.Range(-maxRotationSpeed, maxRotationSpeed);
             }
 
-            Destroy( snowflake, 10.0f );
+            Destroy( snowflake, 20.0f );
 
             yield return new WaitForSeconds( spawnInterval );
         }
